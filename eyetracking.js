c65d015f3f5d1b5cd542d51775d23e9d7ba0babd
Ludvig "Support" Andersson <Ludvand@student.chalmers.se>
2020-10-12 10:53:36 +0200

    Change names
    Moving some of the files around and swapping names

---
+++ eyetracking.js
@@ -0,0 +1,45 @@
+window.onload = async function() {
+
+    webgazer.params.showVideoPreview = true;
+    //start the webgazer tracker
+    await webgazer.setRegression('ridge') /* currently must set regression and tracker */
+        //.setTracker('clmtrackr')
+        .setGazeListener(function(data, clock) {
+           console.log(data); /* data is an object containing an x and y key which are the x and y prediction coordinates (no bounds limiting) */
+          //   console.log(clock); /* elapsed time in milliseconds since webgazer.begin() was called */
+        }).begin();
+        webgazer.showPredictionPoints(true); /* shows a square every 100 milliseconds where current prediction is */
+    
+    
+    //Set up the webgazer video feedback.
+    var setup = function() {
+    
+        //Set up the main canvas. The main canvas is used to calibrate the webgazer.
+        var canvas = document.getElementById("plotting_canvas");
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        canvas.style.position = 'fixed';
+    };
+    setup();
+    
+    };
+    
+    // Kalman Filter defaults to on. Can be toggled by user.
+    window.applyKalmanFilter = true;
+    
+    // Set to true if you want to save the data even if you reload the page.
+    window.saveDataAcrossSessions = true;
+    
+    window.onbeforeunload = function() {
+    webgazer.end();
+    }
+
+document.getElementById("stop").onclick = function() {myFunction()};
+
+function change() {
+    var btn = document.getElementById("stop");
+   
+    btn.innerHTML = "You got it!";
+
+    webgazer.end();
+}
